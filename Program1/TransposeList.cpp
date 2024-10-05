f7cb7dba733c8322fe6a5906fa93973636532f45
bincha2 <davidhkim04@gmail.com>
2024-10-05 01:57:59 -0700

    Commit 5, LLStats + driver Add more unit tests after ensuring driver + LLStats works Transpose + MTF logic are correct for now? Test further

--- Program1/TransposeList.cpp
+++ Program1/TransposeList.cpp
@@ -12,37 +12,24 @@ bool TransposeList::contains(int anEntry)
         {
             if (curr != dummy_header->next) 
             {
-                // Swap curr with its previous node
-                DListNode* prev_node = curr->prev;
+                DListNode *prev_node = curr->prev;
 
-                // Re-link the nodes to swap curr with prev_node
                 prev_node->next = curr->next;
-                if (curr->next != dummy_header) 
-                { // Check if not the dummy header
-                    curr->next->prev = prev_node;
-                }
+                curr->next->prev = prev_node;
 
                 curr->next = prev_node;
                 curr->prev = prev_node->prev;
-
-                if (prev_node->prev != dummy_header) 
-                {
-                    prev_node->prev->next = curr; 
-                }
+                prev_node->prev->next = curr;
                 prev_node->prev = curr;
-                prev_node->next = curr->next;
 
-                if (curr == dummy_header->next) 
+                if (prev_node == dummy_header->next) 
                 {
-                    dummy_header->next = prev_node; 
+                    dummy_header->next = curr; 
                 }
+            }
             return true; 
-        } 
-        else
-        {
-            curr = curr->next;
         }
+        curr = curr->next;
     }
-    return false;
-    }
+    return false; 
 }
